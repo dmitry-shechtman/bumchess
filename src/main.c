ef5a67424988c99f86748127c467aefb2e030f14
Dmitry Shechtman <damnian@gmail.com>
2022-01-14 16:50:21 +0200

    Add Count_Chars and Count_Moves

--- src/main.c
+++ src/main.c
@@ -80,13 +80,16 @@ enum Vec {
 };
 
 enum Count {
-	Count_Colors    =   2,
-	Count_Castlings =   4,
-	Count_Type4     =  16,
+	Count_Colors    =    2,
+	Count_Castlings =    4,
+	Count_Type4     =   16,
 
-	Count_Ranks     =   8,
-	Count_Files     =   8,
-	Count_Squares   = 128,
+	Count_Ranks     =    8,
+	Count_Files     =    8,
+	Count_Squares   =  128,
+
+	Count_Chars     = 1024,
+	Count_Moves     = 1024,
 };
 
 typedef uint8_t piece_t;
@@ -545,7 +548,7 @@ void move_unmake(move_t move) {
 	set_sec(move.sec.from);
 }
 
-extern char buffer[1024];
+extern char buffer[Count_Chars];
 
 uint64_t perft(move_t* moves, uint8_t depth, uint8_t div, char* str);
 char* move_write(char* str, move_t move);
@@ -886,8 +889,8 @@ char* move_write(char* str, move_t move) {
 	return str;
 }
 
-char buffer[1024];
-move_t moves[1024];
+char buffer[Count_Chars];
+move_t moves[Count_Moves];
 
 const char* read_uint8(const char* str, uint8_t* result) {
 	unsigned int i = 0;
